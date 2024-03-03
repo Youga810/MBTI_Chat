c914aee4fd774e4d1dfc54b301f437bd4d8a0856
Youga810 <pron.youg810@gmail.com>
2024-03-04 03:58:20 +0900

    Initial Commit

---
+++ mbti_chat/mbti_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  mbti_chatApp.swift
+//  mbti_chat
+//
+//  Created by hayashiyuga on 2024/03/04.
+//
+
+import SwiftUI
+
+@main
+struct mbti_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
